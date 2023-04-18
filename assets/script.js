29f64534f1c13621a510a0b1bb7e03442432775e
Lee Webb <leewerbb55@hotmail.com>
2023-04-19 02:02:41 +0800

    initialised memory space for key IDs and API key

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+// Grab key IDs and commit them for jQuery, get the API key loaded.
+$(document).ready(function () {
+    const apiKey = '82dc1b6895b734e330686061fc27b46f';
+    const historyList = $('#history');
+    const searchForm = $('#search-form');
+    const searchInput = $('#search-input');
+    const today = $('#today');
+    const forecast = $('#forecast');
